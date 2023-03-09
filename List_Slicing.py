62e8fe4e05da5925b9ab700296e0840314cc5de4
sahanivinay <99119816+sahanivinay@users.noreply.github.com>
2023-03-10 01:00:46 +0530

    Update List_Slicing.py

--- List_Slicing.py
+++ List_Slicing.py
@@ -5,21 +5,21 @@ list = [10,20,30,40,50,60,70,80,90,100]
 print("Original List: ",list)
 
 # This line prints element from 0th Index to fifth element and takes  2 steps
-print("This line prints element from 0th Index to fifth element and takes  2 steps\n")
+print("\nThis line prints element from 0th Index to fifth element and takes  2 steps")
 print(list[0:5:2])
 
 # This line prints element from 0th Index to -1 (i.e. last element)
-print("This line prints element from 0th Index to -1 (i.e. last element)\n")
+print("\nThis line prints element from 0th Index to -1 (i.e. last element)")
 print(list[0:-1])
 
 # This line prints element from 0th Index to the last element as Nth index is not specified
-print("This line prints element from 0th Index to the last element as Nth index is not specified\n")
+print("\nThis line prints element from 0th Index to the last element as Nth index is not specified")
 print(list[0:])
 
 # This line prints element from -1 Index to 10th elements (i.e. -1 = Nth index)
-print("This line prints element from -1 Index to 10th elements (i.e. -1 = Nth index)\n")
+print("\nThis line prints element from -1 Index to 10th elements (i.e. -1 = Nth index)")
 print(list[-1:10])
 
 # This line prints element from 5th Index to 10th element and takes one step
-print("This line prints element from 5th Index to 10th element and takes one step\n")
+print("\nThis line prints element from 5th Index to 10th element and takes one step")
 print(list[5:10:1])
